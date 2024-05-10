1258e06098d3694ca5d4c637b2d57bae69e1c538
Olivier Burggraaff <olivierburggraaff@gmail.com>
2024-05-10 21:53:42 +0200

    Add note about filtering

--- pnn/aggregate.py
+++ pnn/aggregate.py
@@ -9,6 +9,7 @@ import pandas as pd
 def aggregate_uncertainty(results: dict[str, pd.DataFrame]) -> pd.DataFrame:
     """
     Aggregate the uncertainty in given dataframes.
+    Note: previous version of heatmaps filtered entries <0 and >200/>1000.
     """
     results_agg = {key: df.groupby(level=0).mean() for key, df in results.items()}
     results_agg = pd.concat(results_agg)
