8f8c03cac391a0496b98eb442eff765c897fb965
Olivier Burggraaff <olivierburggraaff@gmail.com>
2024-07-06 20:11:24 +0200

    Fix mistake: tanh -> relu

--- pnn/nn/bnn_mcd.py
+++ pnn/nn/bnn_mcd.py
@@ -1,5 +1,5 @@
 """
-Bayesian Neural Network with Monte Carlo Dropout (RNN MCD).
+Bayesian Neural Network with Monte Carlo Dropout (BNN MCD).
 """
 from functools import partial
 from typing import Iterable, Optional
@@ -17,7 +17,7 @@ from .. import constants as c
 
 ### ARCHITECTURE
 def build(input_shape: tuple, *, output_size: int=6,
-          hidden_units: int=50, n_layers: int=5, dropout_rate: float=0.25, l2_reg: float=1e-3, activation="tanh") -> Model:
+          hidden_units: int=50, n_layers: int=5, dropout_rate: float=0.25, l2_reg: float=1e-3, activation="relu") -> Model:
     """
     Construct a BNN with MCD based on the input parameters.
     """
