069dfadb8a342dca6574089268263a2a420762d5
Olivier Burggraaff <olivierburggraaff@gmail.com>
2024-07-08 01:01:54 +0200

    MDN

--- pnn/nn/_select.py
+++ pnn/nn/_select.py
@@ -5,12 +5,14 @@ Cannot be in .common because of circular imports.
 from .bnn_dc import BNN_DC
 from .bnn_mcd import BNN_MCD
 from .ens import Ensemble
+from .mdn import MDN
 from .rnn import RNN_MCD
 from .. import constants as c
 
 nns = {c.bnn_dc: BNN_DC,
        c.bnn_mcd: BNN_MCD,
        c.ensemble: Ensemble,
+       c.mdn: MDN,
        c.rnn: RNN_MCD,}
 
 def select_nn(name: str | c.Parameter):
